4f95867f48f199b0ba16211ec142ae9daffab16c
NTB <134340504+OS-NTB@users.noreply.github.com>
2023-10-21 19:09:23 -0400

    Add files via upload
    Fixed an issue.

--- smb_search.py
+++ smb_search.py
@@ -5,13 +5,12 @@ import pathlib
 import re
 import sys
 import datetime
-from optparse import OptionParser
+import argparse
 
 class SMBSearch:
     def __init__(self, server, username, password, domain=''):
         self.server = server
         self.port = 445
-        self.time = datetime.datetime.now()
         self.conn = SMBConnection(username, password, "name", server, domain, use_ntlm_v2=True, sign_options=SMBConnection.SIGN_WHEN_SUPPORTED, is_direct_tcp=True)
         self.suspicious_filetypes = {
             "Password Document":"^.*(password|passwords).*\.(txt|rtf|docx|xlsx|pdf|xls|doc)$",
@@ -84,9 +83,14 @@ class SMBSearch:
             "Apache Configuration File":"^.*\.htaccess$",
             "Apache Basic Authentication File":"^.*\.htpasswd$"
         }  
+    
+    @property
+    def time(self):
+        time = datetime.datetime.now()
+        return(f'[{time.strftime("%m")}-{time.strftime("%d")}-{time.strftime("%Y")} {time.strftime("%X")}]')
 
     def scan(self, download:bool):
-        print(f'[{self.time.strftime("%m")}-{self.time.strftime("%d")}-{self.time.strftime("%Y")} {self.time.strftime("%X")}] Trying to connect to {self.server}')
+        print(f'{self.time} Trying to connect to {self.server}')
         try:
             #Connect to server using SMB protocol
             self.conn.connect(self.server, self.port)
@@ -97,8 +101,11 @@ class SMBSearch:
             #For loop that finds suspicious files in each share
             for share in shares:
                 share_name = share.name
-                print(f'[{self.time.strftime("%m")}-{self.time.strftime("%d")}-{self.time.strftime("%Y")} {self.time.strftime("%X")}] Scanning \\\{self.server}\{share_name}')
-                self.locate_suspicious(self.conn, share_name, self.server) 
+                print(f'{self.time} Scanning \\\{self.server}\{share_name}')
+                try:
+                    self.locate_suspicious(self.conn, share_name, self.server)
+                except:
+                    print(f'{self.time} Cannot connect to share: \\\\{self.server}\{share_name}')
                 self.cred_hunt(self.conn, share_name, self.server)
                 if download == True:
                     self.download_files(self.conn, share_name, self.server)
@@ -110,21 +117,26 @@ class SMBSearch:
             print(f'[{self.time.strftime("%m")}-{self.time.strftime("%d")}-{self.time.strftime("%Y")} {self.time.strftime("%X")}] Cannot connect to {self.server}')
     
     def locate_suspicious(self, connection, share_name, server, remote_path="/", local_path=""):        
-        try:
-            directory_contents = connection.listPath(share_name, remote_path)
-            for entry in directory_contents:
-                remote_file = os.path.join(remote_path, entry.filename)
-                local_file = os.path.join(local_path, entry.filename)
+        directory_contents = connection.listPath(share_name, remote_path)
+        for entry in directory_contents:
+            remote_file = os.path.join(remote_path, entry.filename)
+            local_file = os.path.join(local_path, entry.filename)
 
-                if not entry.isDirectory:                                   
+            if not entry.isDirectory:
+                try:                                   
                     for key,value in self.suspicious_filetypes.items():
-                        if re.match(value, entry.filename):
+                        if re.match(value, entry.filename, re.IGNORECASE):
                             filepath = remote_file.replace('/','\\')
-                            print(f'[{self.time.strftime("%m")}-{self.time.strftime("%d")}-{self.time.strftime("%Y")} {self.time.strftime("%X")}] Found suspicious {key}: \\\{server}\{share_name}{filepath}')
-                elif entry.filename not in ['.','..']:
+                            print(f'{self.time} Found suspicious {key}: \\\{server}\{share_name}{filepath}')
+                except:
+                    print(f'{self.time} Cannot access: \\\{server}\{share_name}\{entry.filename}')
+            elif entry.isDirectory and entry.filename not in ['.','..']:
+                try:
                     self.locate_suspicious(connection, share_name, server, remote_file, local_file)
-        except:
-            print(f'[{self.time.strftime("%m")}-{self.time.strftime("%d")}-{self.time.strftime("%Y")} {self.time.strftime("%X")}] No access to \\\{server}\{share_name}')
+                except:
+                    path = remote_file.replace('/','\\')
+                    print(f"{self.time} Cannot access: \\\{server}\{share_name}{path}")
+
 
     def cred_hunt(self, connection, share_name, server, remote_path = "/", local_path=""):
         try:
@@ -145,7 +157,7 @@ class SMBSearch:
                                     if re.findall(value, content):
                                         found = re.findall(value,content)
                                         filepath = remote_file.replace('/','\\')
-                                        print(f'[{self.time.strftime("%m")}-{self.time.strftime("%d")}-{self.time.strftime("%Y")} {self.time.strftime("%X")}] Found possible {key} in \\\{server}\{share_name}{filepath}: {found[0]}')
+                                        print(f'{self.time} Found possible {key} in \\\{server}\{share_name}{filepath}: {found[0]}')
                 elif entry.filename not in ['.','..']:
                     self.cred_hunt(connection, share_name, server, remote_file, local_file)
 
@@ -166,43 +178,42 @@ class SMBSearch:
                             filepath = remote_file.replace("/", "\\")
                             with open(local_file, 'wb') as file:
                                 connection.retrieveFile(share_name, remote_file, file)
-                            print(f'[{self.time.strftime("%m")}-{self.time.strftime("%d")}-{self.time.strftime("%Y")} {self.time.strftime("%X")}] Downloaded: \\\{server}\{share_name}{filepath}')
+                            print(f'{self.time} Downloaded: \\\{server}\{share_name}{filepath}')
                 elif entry.filename not in ['.', '..']:
                     self.download_files(connection, remote_file, local_file, share_name)
         except:
             print('', end='')
 
-
 def main(argv):
-    parser = OptionParser()
-    parser.add_option('-u', '--username', help="Username that will be used for authentication. Leave blank for anonymous authentication", default="", dest="username")
-    parser.add_option('-p', '--password', help="Password that will be used for authentication. Leave Blank for anonymous authentication.", default="", dest="password")
-    parser.add_option('-i', '--ip', help="Target IP address", dest="ip")
-    parser.add_option('-l', '--list', help="List of IP Addresses", default="", dest="list")
-    parser.add_option('-d', '--domain', help="Domain Name", dest="domain", default="")
-    parser.add_option('-g', '--get', help='Download suspicious files', dest="download", action="store_true", default=False)
-    (options, args) = parser.parse_args()
+    parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=52))
+    parser.add_argument('-u', '--user', help="Set the username", default="", metavar='USER', dest="username")
+    parser.add_argument('-p', '--pass', help="Set the password", default="", metavar='PASS', dest="password")
+    parser.add_argument('-i', '--ip', help="Use this IP to connect to", default="", dest="ip")
+    parser.add_argument('-l', '--list', help="Use this list of IPs to connect to", default="", dest="list")
+    parser.add_argument('-d', '--domain', help="Set the domain", default="", dest="domain")
+    parser.add_argument('-g', '--get', help="Use this option to download suspicious files", dest="download", action="store_true", default=False)
+    args = parser.parse_args()
 
-    if options.list != '':
-        with open(options.list, 'r') as file:
+    if args.list != '':
+        with open(args.list, 'r') as file:
             lines = file.readlines()
             for entry in lines:
                 entry = entry.strip()
-                if options.domain != '':
-                    smb = SMBSearch(entry, options.username, options.password, options.domain)
-                    smb.scan(options.download)
+                if args.domain != '':
+                    smb = SMBSearch(entry, args.username, args.password, args.domain)
+                    smb.scan(args.download)
                 else:
-                    smb = SMBSearch(entry, options.username, options.password)
-                    smb.scan(options.download)
+                    smb = SMBSearch(entry, args.username, args.password)
+                    smb.scan(args.download)
     else:
-        if options.domain != '':
-            smb = SMBSearch(options.ip, options.username, options.password, options.domain)
-            smb.scan(options.download)
+        if args.domain != '':
+            smb = SMBSearch(args.ip, args.username, args.password, args.domain)
+            smb.scan(args.download)
         else:
-            smb = SMBSearch(options.ip, options.username, options.password)
-            smb.scan(options.download)
+            smb = SMBSearch(args.ip, args.username, args.password)
+            smb.scan(args.download)
     
-    print(f'[{smb.time.strftime("%m")}-{smb.time.strftime("%d")}-{smb.time.strftime("%Y")} {smb.time.strftime("%X")}] Scan completed.')
+    print(f'{smb.time} Scan completed.')
 
 if __name__ == '__main__':
     main(sys.argv[1:])
